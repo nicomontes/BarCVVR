2d45020fa276b4457b5ec2c556bc96bc2ec760c4
nmontes <nicolas.montes@telecom-sudparis.eu>
2018-08-27 19:08:50 +0200

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,7 @@ class UsersController < ApplicationController
       Operation.where(user_id: user.id).find_each do |operation|
         @operationTotal[user.id] = @operationTotal[user.id] + operation.sum
       end
-      @operationTotal[user.id] = @operationTotal[user.id] + user.amount
-      User.find(user.id).update_attribute(:initAmount, user.amount)
+      @operationTotal[user.id] = @operationTotal[user.id] + user.initAmount
       @totalAmount = @totalAmount + @operationTotal[user.id]
       totalOperationLastMouth = 0
       Operation.where(user_id: user.id).where.not('numberDrink' => nil).where("created_at > ?", Date.today.last_month()).find_each do |operation|
